c38a21ba036edab66ecf79112267638a1800e50e
Alejandro Pirola <alejamp@gmail.com>
2024-09-08 05:04:02 -0300

    fix chromadb storage tests

--- tests/text2vec/openai_test.py
+++ tests/text2vec/openai_test.py
@@ -17,7 +17,7 @@ texts=[
 @pytest.fixture
 def client():
     text2vec = CachedOpenAIEmbedding()
-    return ChromaStore(text2vec, collection_name='test_collection')
+    return ChromaStore(text2vec, collection_name='test_openai_collection')
 
 
     
