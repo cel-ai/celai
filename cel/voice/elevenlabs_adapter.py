c89d90dc028c069e191c4b66c401aa34034e676d
Bruno Hernandez <hernandezbg@gmail.com>
2024-11-16 18:38:25 -0300

    Ability to specify the voice model directly in the configuration

--- cel/voice/elevenlabs_adapter.py
+++ cel/voice/elevenlabs_adapter.py
@@ -5,18 +5,19 @@ from cel.voice.base_voice_provider import BaseVoiceProvider
 
 
 # https://help.elevenlabs.io/hc/en-us/articles/17883183930129-What-models-do-you-offer-and-what-is-the-difference-between-them
-DEFAULT_MODEL="eleven_turbo_v2_5"
 
 class ElevenLabsAdapter(BaseVoiceProvider):
     
     def __init__(self, api_key: str = None, 
                  default_voice: str = "N2lVS1w4EtoT3dr4eOWO", 
+                 model: str = "eleven_turbo_v2_5",
                  settings: VoiceSettings = None,
                  preprocessing_callback: callable = None
                 ):
         
         self.api_key = api_key
         self.default_voice = default_voice
+        self.default_model = model
         self.default_voice_settings = settings or VoiceSettings(
                                                         stability=0.50, 
                                                         similarity_boost=0.75, 
@@ -39,7 +40,7 @@ class ElevenLabsAdapter(BaseVoiceProvider):
 
         audio = self.client.generate(
             text=text,
-            model=DEFAULT_MODEL,
+            model=self.default_model,
             voice=Voice(
                 voice_id=voice or self.default_voice,
                 settings=settings or self.default_voice_settings,
@@ -61,6 +62,7 @@ if __name__ == "__main__":
     ELEVENLABS_API_KEY= os.getenv("ELEVENLABS_API_KEY")
     tts = ElevenLabsAdapter(
         api_key=ELEVENLABS_API_KEY,
+        model=os.getenv("ELEVENLABS_MODEL", "eleven_turbo_v2_5"),
         preprocessing_callback=lambda x: x + " - from ElevenLabs"
     )
     audio = tts.TTS("This is a test")
